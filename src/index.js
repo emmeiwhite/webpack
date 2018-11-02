3d769c467b790a9baf2be6e34e69b4abe0f4d958
emmeiwhite <emmeiwhite@gmail.com>
2018-11-02 21:01:16 +0530

    webpack@4 successfully generated a bundled single file main.js in dist folder automatically

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Book from './book';
+
+const book1 = new Book('Tales of Oceans',990);
+const book2 = new Book('Paradise is a reality',1750);
+
+console.log(book1.name);
+console.log(book2.name);
